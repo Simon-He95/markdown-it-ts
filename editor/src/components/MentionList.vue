4ae509b8ce633abe11a4cfeb5b34a9588880a511
Simon He <674949287@qq.com>
2025-12-02 18:48:03 +0800

    fix: resolve issue with item selection in MentionList component

--- editor/src/components/MentionList.vue
+++ editor/src/components/MentionList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, defineProps, defineExpose, ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 interface MentionListItem {
   id: string
